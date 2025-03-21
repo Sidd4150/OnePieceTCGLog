efdc3ac857b41f3c2331a1b96c1308636bc80269
Siddhartha Shakya <sshakya@dons.usfca.edu>
2025-03-21 12:31:11 -0700

    Logo fix

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,7 +4,7 @@ export default function Nav() {
 
     return (
         <nav className="Nav" >
-            <img className="Logo" src="public/assets/newLogo.png" alt="Logo"></img>
+            <img className="Logo" src="/assets/newLogo.png" alt="Logo"></img>
             <ul className="pages">
                 <li> <a href="#">Home</a></li>
                 <li> <a href="#">About</a></li>
